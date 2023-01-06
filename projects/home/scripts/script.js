02751a24dfa14fb6c17c1aa78de7b8a148ac0e07
acctg-analyst <cameron.taylor2472@yahoo.com>
2023-01-06 13:33:55 -0700

    Update code for efficiency

--- projects/home/scripts/script.js
+++ projects/home/scripts/script.js
@@ -1,15 +1,10 @@
 const updatedDate = document.querySelector('#updatedDate');
 
 const date = new Date();
-let day = date.getDate();
-let month = date.getMonth()+1;
 let year = date.getFullYear();
-let hours = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
 
-let currentDate = `Last Updated: ${month}/${day}/${year} ${hours}:${minutes}:${seconds}`;
 
+let currentDate = `Last Updated: ${document.lastModified}`
 updatedDate.textContent = currentDate;
 
-document.querySelector('#currentYear').textContent = `@ ${year} | Cameron M. Taylor | Idaho`
+document.querySelector('#currentYear').innerHTML = `&copy ${year} .:|:. Cameron M. Taylor .:|:. Idaho`
