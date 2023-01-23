fbd160e814a161d7c12d4e0e747fa137e4996d0b
acctg-analyst <cameron.taylor2472@yahoo.com>
2023-01-23 15:49:15 -0700

    Presentation exp & class exp

---
+++ lesson4/scripts/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.btn');
+
+function show() {
+    button.classList.add('newstyle');
+}
+
+button.addEventListener('click', show);
+
---
+++ presentation/main.js
@@ -0,0 +1,27 @@
+//1. Selecting an element
+const h1 = document.querySelector('#mainHeading');
+h1. innerText = "Hello Class";
+
+
+//2. Creat/Edit/Add
+// const p = document.createElement('p');
+// p.innerHTML = "Hello Class";
+// document.body.appendChild(p);
+
+//3. Removing elements from the body
+// document.querySelector('h1').remove();
+
+//4. Adding Style to the documet
+// p.style.color = "red";
+
+//5. Adding Event Listeners
+// const btn = document.createElement('button');
+// btn.innerText = "Press Me";
+// document.body.appendChild(btn);
+// btn.addEventListener('click', () => {
+//     const p2 = document.createElement('p');
+//     p2.innerHTML = "The button made me";
+//     p2.style.backgroundColor = "black";
+//     p2.style.color = "white";
+//     document.body.appendChild(p2);
+// })
