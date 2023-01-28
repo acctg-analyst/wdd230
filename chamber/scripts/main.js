11091caba1799b21668f0481b72bad1c7ff9c85f
acctg-analyst <cameron.taylor2472@yahoo.com>
2023-01-27 21:45:55 -0700

    Fixing javascript for nav changes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -7,7 +7,7 @@ const options = {
     year: "numeric"
 };
 
-today.textContent = `${new Date().toLocaleDateString("en-UK",options)}`;
+today.textContent = `${new Date().toLocaleDateString("en-UK", options)}`;
 
 
 const lastModified = document.querySelector('#lastModified');
@@ -19,7 +19,7 @@ let year = date.getFullYear();
 console.log(year);
 
 
-document.querySelector('#currentYear').innerHTML =  `&copy;${year} Treasure Valley Chamber `
+document.querySelector('#currentYear').innerHTML = `&copy;${year} Treasure Valley Chamber `
 
 
 // when hamburger is hit
@@ -28,74 +28,77 @@ const hamburger = document.querySelector('.ham');
 
 let clickCounter = 1;
 hamburger.addEventListener('click', () => {
-    
-    clickCounter = clickCounter+1
 
-    if (clickCounter % 2 == 0) {
+    clickCounter = clickCounter + 1
 
+    if (clickCounter % 2 == 0 && !(document.querySelector('#navContainer').classList.contains('large'))) {
+        document.querySelector('#navContainer').classList.toggle('on');
         const navDiv = document.querySelector('#navContainer');
         const navBar = document.createElement('nav');
         navDiv.appendChild(navBar);
 
         const a1 = document.createElement('a');
-        a1.setAttribute("href","#");
+        a1.setAttribute("href", "#");
         a1.textContent = "Discover";
         navBar.appendChild(a1);
 
         const a2 = document.createElement('a');
-        a2.setAttribute("href","#");
+        a2.setAttribute("href", "#");
         a2.textContent = "Directory";
         navBar.appendChild(a2);
 
         const a3 = document.createElement('a');
-        a3.setAttribute("href","#");
+        a3.setAttribute("href", "#");
         a3.textContent = "Join";
         navBar.appendChild(a3);
 
         const a4 = document.createElement('a');
-        a4.setAttribute("href","#");
+        a4.setAttribute("href", "#");
         a4.textContent = "Contact";
         navBar.appendChild(a4);
-        
+
     }
 
     else {
         document.querySelector('nav').remove()
+        document.querySelector('#navContainer').classList.toggle('on');
     }
 });
 
 function reportWindowSize() {
-    if (window.innerWidth >= 800 && document.querySelector('#navContainer').className != 'on'){
+    if (window.innerWidth >= 940 && !(document.querySelector('#navContainer').classList.contains('on'))) {
         document.querySelector('#navContainer').classList.toggle('on');
+        document.querySelector('#navContainer').classList.toggle('large');
         const navDiv = document.querySelector('#navContainer');
         const navBar = document.createElement('nav');
         navDiv.appendChild(navBar);
 
         const a1 = document.createElement('a');
-        a1.setAttribute("href","#");
+        a1.setAttribute("href", "#");
         a1.textContent = "Discover";
         navBar.appendChild(a1);
 
         const a2 = document.createElement('a');
-        a2.setAttribute("href","#");
+        a2.setAttribute("href", "#");
         a2.textContent = "Directory";
         navBar.appendChild(a2);
 
         const a3 = document.createElement('a');
-        a3.setAttribute("href","#");
+        a3.setAttribute("href", "#");
         a3.textContent = "Join";
         navBar.appendChild(a3);
 
         const a4 = document.createElement('a');
-        a4.setAttribute("href","#");
+        a4.setAttribute("href", "#");
         a4.textContent = "Contact";
         navBar.appendChild(a4);
     }
-    else if (window.innerWidth < 800 && document.querySelector('#navContainer').className == 'on'){
+    else if (window.innerWidth < 940 && document.querySelector('#navContainer').classList.contains('on') && document.querySelector('#navContainer').classList.contains('large')) {
         document.querySelector('#navContainer').classList.toggle('on');
+        document.querySelector('#navContainer').classList.toggle('large');
         document.querySelector('nav').remove()
     }
-  }
+}
 
 window.addEventListener('resize', reportWindowSize);
 
