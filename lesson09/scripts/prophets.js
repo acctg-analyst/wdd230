04208299ef2ea7ac043df9035f43648d069afb1f
acctg-analyst <cameron.taylor2472@yahoo.com>
2023-02-27 16:23:14 -0700

    some fixes on the join page and prophets api

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+  
+getProphetData();
+
+
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards');
+
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let dob = document.createElement('p');
+        let picture = document.createElement('img');
+
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        picture.setAttribute('src', `${prophet.imageurl}`);
+        picture.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President` );
+        picture.setAttribute('loading', 'lazy');
+        dob.textContent = `${prophet.birthdate}`
+
+        card.appendChild(h2);
+        card.appendChild(dob);
+        card.appendChild(picture);
+        
+
+        cards.appendChild(card);
+
+    })
+}
+
