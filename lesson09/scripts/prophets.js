0767608b9956ffa8d21fb9a2116b1411d58d2a7f
acctg-analyst <cameron.taylor2472@yahoo.com>
2023-02-27 16:27:20 -0700

    fixing content on cards

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -18,16 +18,19 @@ const displayProphets = (prophets) => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let dob = document.createElement('p');
+        let pob = document.createElement('p');
         let picture = document.createElement('img');
 
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
         picture.setAttribute('src', `${prophet.imageurl}`);
         picture.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} - ${prophet.order} Latter-day President` );
         picture.setAttribute('loading', 'lazy');
-        dob.textContent = `${prophet.birthdate}`
+        dob.textContent = `Date of Birth: ${prophet.birthdate}`;
+        pob.textContent = `Place of Birth: ${prophet.birthplace}`;
 
         card.appendChild(h2);
         card.appendChild(dob);
+        card.appendChild(pob);
         card.appendChild(picture);
         
 
